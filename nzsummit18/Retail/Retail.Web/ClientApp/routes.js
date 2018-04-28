a504c7b679ff3a43453eb25eabc0b55832559765
Matt Simpson <masim@microsoft.com>
2018-04-28 21:07:18 +1200

    fix repo

--- nzsummit18/Retail/Retail.Web/ClientApp/routes.js
+++
@@ -1,7 +0,0 @@
-import Shop from 'components/shop'
-import HomePage from 'components/home-page'
-
-export const routes = [
-    { path: '/', component: HomePage, display: 'Home', style: 'glyphicon glyphicon-home' },
-    { path: '/shop', component: Shop, display: 'Shop', style: 'glyphicon glyphicon-shopping-cart' }
-]
