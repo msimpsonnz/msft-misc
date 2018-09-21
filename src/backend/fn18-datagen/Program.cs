b345aca18863773d9d2b98d7437f42562f6f5593
Matt Simpson <masim@microsoft.com>
2018-09-21 13:21:47 +1200

    Added generator and gitignore

---
+++ src/backend/fn18-datagen/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Bogus;
+
+namespace fn18_datagen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fakes = Fake();
+
+        }
+
+
+        static void Fake()
+        {
+            var faker = new Faker("en");
+            var o = new Order()
+            {
+                OrderId = faker.Random.Number(1, 100),
+                Item = faker.Lorem.Sentence(),
+                Quantity = faker.Random.Number(1, 10)
+            };
+            o.Dump();
+        }
+
+
+    }
+}
