4d366d8a69a0cd6d33ce09a5dec4b37d2ec8c685
Matt Simpson <masim@microsoft.com>
2018-09-28 07:27:48 +1200

    updated signalr

--- src/backend/fn18-signalr/GetData.cs
+++ src/backend/fn18-signalr/GetData.cs
@@ -17,27 +17,13 @@ namespace fn18_signalr
     public static class GetData
     {
         [FunctionName("GetData")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB("db", "coll", ConnectionStringSetting = "AzureWebJobsCosmosDBConnectionString", SqlQuery = "SELECT top 2 * FROM c")] IEnumerable<object> docs,
+            ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            dynamic flight = new ExpandoObject();
-            flight.id = 111;
-            flight.from = "SEA";
-            flight.to = "YVR";
-            flight.price = 199;
-
-            dynamic flight2 = new ExpandoObject();
-            flight2.id = 112;
-            flight2.from = "AKL";
-            flight2.to = "WLG";
-            flight2.price = 199;
-
-            List<dynamic> flights = new List<dynamic>();
-            flights.Add(flight);
-            flights.Add(flight2);
-
-            return (ActionResult)new OkObjectResult(flights);
+            return new OkObjectResult(docs);
         }
     }
 }
---
+++ src/frontend/FN18.Frontend/FN18.Frontend/Models/NasaImageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FN18.Frontend.Models
+{
+    public class Info
+    {
+        public string Type { get; set; }
+        public string Sentiment { get; set; }
+        public int CommentId { get; set; }
+        public string CommentUrl { get; set; }
+        public string Text { get; set; }
+        public string id { get; set; }
+    }
+
+}
