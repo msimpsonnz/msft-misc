5bdb539be4a83391bcd0fc9dc80565efa8bef0c7
Matt Simpson <masim@microsoft.com>
2018-09-28 17:13:00 +1200

    content mod with Razor

---
+++ src/frontend/FN18.Web/Helpers/ModeratorHelper.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Azure.CognitiveServices.ContentModerator;
+using Microsoft.CognitiveServices.ContentModerator;
+using Microsoft.CognitiveServices.ContentModerator.Models;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FN18.Web.Helpers
+{
+    /// <summary>
+    /// Wraps the creation and configuration of a Content Moderator client.
+    /// </summary>
+    /// <remarks>This class library contains insecure code. If you adapt this 
+    /// code for use in production, use a secure method of storing and using
+    /// your Content Moderator subscription key.</remarks>
+    public static class ModeratorHelper
+    { 
+        /// <summary>
+        /// Returns a new Content Moderator client for your subscription.
+        /// </summary>
+        /// <returns>The new client.</returns>
+        /// <remarks>The <see cref="ContentModeratorClient"/> is disposable.
+        /// When you have finished using the client,
+        /// you should dispose of it either directly or indirectly. </remarks>
+        public static ContentModeratorClient contentModeratorClient(string AzureBaseURL, string CMSubscriptionKey)
+        {
+            // Create and initialize an instance of the Content Moderator API wrapper.
+            ContentModeratorClient client = new ContentModeratorClient(new ApiKeyServiceClientCredentials(CMSubscriptionKey));
+
+            client.Endpoint = AzureBaseURL;
+            return client;
+        }
+
+        public static async Task<Screen> TextScreen(ContentModeratorClient client, string text)
+        {
+            try
+            {
+                byte[] byteArray = Encoding.UTF8.GetBytes(text);
+                MemoryStream stream = new MemoryStream(byteArray);
+                var screenResult = await client.TextModeration.ScreenTextAsync("text/plain", stream, "eng", true, true, null, true);
+                return screenResult;
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                throw;
+            }
+
+        }
+    }
+}
---
+++ src/frontend/FN18.Web/Pages/Screen.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FN18.Web.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CognitiveServices.ContentModerator.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace FN18.Web.Pages
+{
+    public class ScreenModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public ScreenModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [TempData]
+        public string text { get; set; }
+        [BindProperty]
+        public Screen ScreenResult { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var client = ModeratorHelper.contentModeratorClient(_configuration["ContentModeratorRegion"], _configuration["ContentModeratorKey"]);
+            ScreenResult = await ModeratorHelper.TextScreen(client, text);
+            return Page();
+        }
+    }
+}
