af9c143429c7a35c9019d1ea636c4e563cebc96a
Matt Simpson <masim@microsoft.com>
2018-10-08 16:31:10 +1300

    added catalog

---
+++ src/FN18.Core/CatalogItem.cs
@@ -0,0 +1,14 @@
+﻿namespace FN18.Core
+{
+    public class CatalogItem
+    { 
+        public string Id { get; set; }
+        public string CatalogBrandId { get; set; }
+        public string CatalogTypeId { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string PictureUri { get; set; }
+        public string Price { get; set; }
+    }
+
+}
