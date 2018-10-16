3798f69cf0aa01560cf0a1f0eb2bd4ce5d6f7293
Matt Simpson <masim@microsoft.com>
2018-10-16 19:59:25 +1300

    added tests

---
+++ MediaServices.Demo.Tests/TestHelper.cs
@@ -0,0 +1,23 @@
+﻿using MediaServices.Demo.Function;
+using Microsoft.Extensions.Configuration;
+
+namespace MediaServices.Demo.Tests
+{
+    public class TestHelper
+    {
+        public static IConfigurationRoot GetIConfigurationRoot()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+        }
+        public static MetaData GetApplicationConfiguration()
+        {
+            var metaDataResult = new MetaData();
+            var config = GetIConfigurationRoot();
+            config.GetSection("result").Bind(metaDataResult);
+            return metaDataResult;
+        }
+    }
+}
